a7b0ceaad27ede261667e3103f27621aa763e891
MrNacatamal09 <acramireza@uamv.edu.ni>
2026-08-31 22:42:26 -0600

    Conecto login con la interfaz de la pulperia

--- Reto1_Inventario/src/main/java/module-info.java
+++ Reto1_Inventario/src/main/java/module-info.java
@@ -2,7 +2,11 @@ module ni.edu.uam.reto1_inventario {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens ni.edu.uam.reto1_inventario to javafx.fxml;
+    opens ni.edu.uam.reto1_inventario.controllers to javafx.fxml;
+    opens ni.edu.uam.reto1_inventario.models to javafx.base;
+    requires static lombok;
+
     exports ni.edu.uam.reto1_inventario;
+    exports ni.edu.uam.reto1_inventario.controllers;
 }
